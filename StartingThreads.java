bffa97bc3132379501fafbb0969184c0635fa118
Larissa Almada <62493810+Lalalmada@users.noreply.github.com>
2026-07-27 17:15:48 +0000

    add files

--- StartingThreads.java
+++ StartingThreads.java
@@ -1,3 +1,13 @@
+public class StartingThreads{
+    public static void main(String[] args){
+        var t1 = new Thread(new Runner1());
+        var t2 = new Thread(new Runner2());
+
+        t1.start();// sempre que se chama o metodo start, se aloca recursos do sistema para o novo thread
+        t2.start(); 
+    }
+}
+
 class Runner1 implements Runnable{
     @Override
     public void run(){
@@ -14,15 +24,5 @@ class Runner2 implements Runnable{
         for(int i=0; i<10;++i){
             System.out.println("Runner2: "+i);
         }
-    }
-}
-
-public class StartingThreads{
-    public static void main(String[] args){
-        var t1 = new Thread(new Runner1());
-        var t2 = new Thread(new Runner2());
-
-        t1.start();// sempre que se chama o metodo start, se aloca recursos do sistema para o novo thread
-        t2.start(); 
     }
 }
