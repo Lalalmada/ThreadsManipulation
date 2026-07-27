4c7959abe9475a70e013e152f6ae45b78801f052
Larissa Almada <62493810+Lalalmada@users.noreply.github.com>
2026-07-27 16:54:38 +0000

    add files

--- SequentialProcess.java
+++ SequentialProcess.java
@@ -1,1 +1,28 @@
 class Runner1{
+    public void execute(){
+        for(int i=0; i<10;++i){
+            System.out.println("Runner1: "+i);
+        }
+    }
+}
+
+class Runner2{
+    public void execute(){
+        for(int i=0; i<10;++i){
+            System.out.println("Runner2: "+i);
+        }
+    }
+}
+
+
+public class App{
+
+    public static void main(String[] args){
+        var runner1 = new Runner1();
+        var runner2 = new Runner2();
+
+        runner1.execute();
+        runner2.execute();
+        
+    }
+}
