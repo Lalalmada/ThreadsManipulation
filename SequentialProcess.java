d60b266d0ac627d5b731c785e884e8f71b45e119
Larissa Almada <62493810+Lalalmada@users.noreply.github.com>
2026-07-27 17:18:47 +0000

    add files

--- SequentialProcess.java
+++ SequentialProcess.java
@@ -1,3 +1,15 @@
+public class SequentialProcess{
+
+    public static void main(String[] args){
+        var runner1 = new Runner1();
+        var runner2 = new Runner2();
+
+        runner1.execute();
+        runner2.execute();// algoritmo time slicing, executa um, executa outro
+        
+    }
+}
+
 class Runner1{
     public void execute(){
         for(int i=0; i<10;++i){
@@ -11,18 +23,5 @@ class Runner2{
         for(int i=0; i<10;++i){
             System.out.println("Runner2: "+i);
         }
-    }
-}
-
-
-public class App{
-
-    public static void main(String[] args){
-        var runner1 = new Runner1();
-        var runner2 = new Runner2();
-
-        runner1.execute();
-        runner2.execute();
-        
     }
 }
